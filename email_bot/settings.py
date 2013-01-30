b404759dca49495cdd12aa635d7f0781c92f181e
Alex Lovell-Troy <alex@lovelltroy.org>
2013-01-30 14:54:04 +0000

    commit and push while dealing with internet outage

--- email_bot/settings.py
+++ email_bot/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
+    'avatar',
     'email_integration',
     'oneleap_mailer',
     'introductions',
